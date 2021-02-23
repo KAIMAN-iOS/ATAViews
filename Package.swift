a3302b99c456b3e01d1d1fed22c6537936160d67
GG <jerometonnelier@yahoo.fr>
2021-02-23 14:58:35 +0100

    .branch("master")

--- Package.swift
+++ Package.swift
@@ -14,9 +14,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/jerometonnelier/KExtensions", from: "1.0.0"),
-        .package(url: "https://github.com/jerometonnelier/ATAConfiguration", from: "1.0.0"),
-        .package(url: "https://github.com/jerometonnelier/TextFieldEffects", from: "1.7.0"),
+        .package(url: "https://github.com/jerometonnelier/KExtensions", .branch("master")),
+        .package(url: "https://github.com/jerometonnelier/ATAConfiguration", .branch("master")),
+        .package(url: "https://github.com/jerometonnelier/TextFieldEffects", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
