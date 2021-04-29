1d27f9d51f475d24e6a414198a39eec28f20e78e
jerometonnelier <jerometonnelier@yahoo.fr>
2021-04-29 17:11:51 +0200

    Selectable button color options

--- Sources/ATAViews/SelectableButton.swift
+++ Sources/ATAViews/SelectableButton.swift
@@ -9,31 +9,33 @@ import UIKit
 
 @IBDesignable
 public class SelectableButton: UIButton {
-    public static var selectedColor: UIColor = .black
-    public static var unselectedColor: UIColor = .white
+    static public var selectedBackgroundColor: UIColor = .black
+    static public var selectedTextColor: UIColor = .white
+    static public var selectedBorderColor: UIColor = .black
+    static public var unselectedBackgroundColor: UIColor = .clear
+    static public var unselectedTextColor: UIColor = .gray
+    static public var unselectedBorderColor: UIColor = .gray
     
-    @IBInspectable
-    public var selectedColor: UIColor?
+    @IBInspectable public var selectedBackgroundColor: UIColor?
+    @IBInspectable public var selectedTextColor: UIColor?
+    @IBInspectable public var selectedBorderColor: UIColor?
+    @IBInspectable public var unselectedBackgroundColor: UIColor?
+    @IBInspectable public var unselectedTextColor: UIColor?
+    @IBInspectable public var unselectedBorderColor: UIColor?
     
-    @IBInspectable
-    public var unselectedColor: UIColor?
-    
-    @IBInspectable
-    public var contentInsets: UIEdgeInsets = UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)  {
+    @IBInspectable public var contentInsets: UIEdgeInsets = UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)  {
         didSet {
             setNeedsLayout()
         }
     }
     
-    @IBInspectable
-    public var buttonCornerRadius: CGFloat = 10.0  {
+    @IBInspectable public var buttonCornerRadius: CGFloat = 10.0  {
         didSet {
             setNeedsLayout()
         }
     }
     
-    @IBInspectable
-    public var borderWidth: CGFloat = 1.0  {
+    @IBInspectable public var borderWidth: CGFloat = 0.5  {
         didSet {
             setNeedsLayout()
         }
@@ -44,15 +46,16 @@ public class SelectableButton: UIButton {
         }
     }
     var hasFocus: Bool = false
-    var focusColor: UIColor { hasFocus ? (selectedColor ?? SelectableButton.selectedColor) : (unselectedColor ?? SelectableButton.unselectedColor) }
-    var unfocusedColor: UIColor { hasFocus == false ? (selectedColor ?? SelectableButton.selectedColor) : (unselectedColor ?? SelectableButton.unselectedColor) }
+    var componentBackgroundColor: UIColor { hasFocus ? (selectedBackgroundColor ?? SelectableButton.selectedBackgroundColor) : (unselectedBackgroundColor ?? SelectableButton.unselectedBackgroundColor) }
+    var textColor: UIColor { hasFocus ? (selectedTextColor ?? SelectableButton.selectedTextColor) : (unselectedTextColor ?? SelectableButton.unselectedTextColor) }
+    var borderColor: UIColor { hasFocus ? (selectedBorderColor ?? SelectableButton.selectedBorderColor) : (unselectedBorderColor ?? SelectableButton.unselectedBorderColor) }
     
     public override func layoutSubviews() {
         super.layoutSubviews()
         layer.borderWidth = borderWidth
-        layer.borderColor = (selectedColor ?? SelectableButton.selectedColor).cgColor
-        backgroundColor = focusColor
-        setTitleColor(unfocusedColor, for: .normal)
+        layer.borderColor = borderColor.cgColor
+        backgroundColor = componentBackgroundColor
+        setTitleColor(textColor, for: .normal)
         layer.cornerRadius = buttonCornerRadius
         contentEdgeInsets = contentInsets
     }
