3e4fa55ee7ca1d237ee9c62e814b3ded184d5df6
jerometonnelier <jerometonnelier@yahoo.fr>
2021-08-25 10:21:25 +0200

    public BorderedErrorView

--- Sources/ATAViews/BorderedErrorView.swift
+++ Sources/ATAViews/BorderedErrorView.swift
@@ -10,15 +10,15 @@ import LabelExtension
 import FontExtension
 import ATAConfiguration
 
-protocol CloseDelegate: NSObjectProtocol {
+public protocol CloseDelegate: NSObjectProtocol {
     func close(_ view: UIView)
 }
 
-class BorderedErrorView: UIView {
-    static func load() -> BorderedErrorView {
+public class BorderedErrorView: UIView {
+    public static func load() -> BorderedErrorView {
         Bundle.module.loadNibNamed("BorderedErrorView", owner: nil, options: nil)!.first! as! BorderedErrorView
     }
-    static var configuration: ATAConfiguration!
+    public static var configuration: ATAConfiguration!
     @IBOutlet weak var closeButton: UIButton!  {
         didSet {
             closeButton.tintColor = BorderedErrorView.configuration.palette.primary
@@ -47,13 +47,13 @@ class BorderedErrorView: UIView {
     }
     
     /// configure with a text and a close button to dimiss tha view
-    func configure(_ text: String, delegate: CloseDelegate) {
+    public func configure(_ text: String, delegate: CloseDelegate) {
         closeDelegate = delegate
         errorLabel.set(text: text, for: .footnote, textColor: BorderedErrorView.configuration.palette.primary)
     }
     
     /// configure only with an error. The closeBUtton is hidden
-    func configure(_ text: String) {
+    public func configure(_ text: String) {
         closeButton.isHidden = true
         errorLabel.set(text: text, for: .footnote, textColor: BorderedErrorView.configuration.palette.primary)
     }
